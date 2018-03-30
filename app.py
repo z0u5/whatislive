06504a86f0331629c7571184df53c9982fba5c28
z0u5 <35923701+z0u5@users.noreply.github.com>
2018-03-30 21:06:46 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
 from flask import Flask
-from datetime import datetime
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
-
     return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
-    <img src="https://www.w3schools.com/howto/img_paris.jpg" />
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Page</title>
+</head>
+<body>
+<div>
+    <center>
+        <h1>
+            Hello you are  in the HELL
+        </h1>
+        <p>-------------------------------------------------------------</p>
+    </center>
+    <h4>Hi, I use this site for my RAT or Backdoor</h4>
+
+</body>
+</html>
     """.format(time=the_time)
 
 if __name__ == '__main__':
