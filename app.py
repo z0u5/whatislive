553ccfff03e982ebd8cf634cc8629b9f6f1d4d01
z0u5 <35923701+z0u5@users.noreply.github.com>
2018-03-30 21:03:36 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-import socket
+from flask import Flask
+from datetime import datetime
+app = Flask(__name__)
 
-host = socket.gethostbyname(socket.gethostname())
+@app.route('/')
+def homepage():
+    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
-s = socket.socket()
-s.bind((host,5000))
-s.listen(10)
-con, addr = s.accept()
-con.send("666 is good")
-s.close()
+    return """
+    <h1>Hello heroku</h1>
+    <p>It is currently {time}.</p>
+    <img src="https://www.w3schools.com/howto/img_paris.jpg" />
+    """.format(time=the_time)
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
